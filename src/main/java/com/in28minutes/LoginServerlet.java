4c6fbcd8a72e3dd662b2b3bc8588e2f0cffb3659
ViktorSjolind <viktor.sjolind@gmail.com>
2017-11-08 01:00:28 +0200

    Simple JSP

---
+++ src/main/java/com/in28minutes/LoginServerlet.java
@@ -0,0 +1,30 @@
+package com.in28minutes;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns="/login.do")
+public class LoginServerlet extends HttpServlet{
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		 * Servlet
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Dummy stuff</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("9/11 was a lie");
+		out.println("</body>");
+		out.println("</html>");
+		*/
+		//path start from web-inf
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		
+	}
+}
